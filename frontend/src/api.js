2caa87041e2131c8f744de39511ab966e031330d
Kishor1703 <pskishor196@gmail.com>
2026-06-04 12:38:55 +0530

    Jenkins added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL = (
-  process.env.REACT_APP_API_BASE_URL?.trim() || "https://hire-ly-izmm.vercel.app/"
+  process.env.REACT_APP_API_BASE_URL?.trim()
 ).replace(/\/+$/, "");
 
 const API = axios.create({
